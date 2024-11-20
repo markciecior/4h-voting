6007a0061e00eca67beacec8732358755a2d3990
Mark Ciecior <mciecior@carrieraccessit.com>
2024-11-20 10:56:26 -0600

    remove print statement

--- django_fingerprint/decorators.py
+++ django_fingerprint/decorators.py
@@ -5,7 +5,6 @@ from django.urls import reverse
 def login_required_fingerprint(view_func):
     def wrapper(request, *args, **kwargs):
         if not request.user.is_authenticated:
-            print("redirecting to fp login")
             return HttpResponseRedirect(
                 f"{reverse("django_fingerprint:fplogin")}?next={request.path}"
             )
